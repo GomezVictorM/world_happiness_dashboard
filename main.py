a982ebf254274b186c978d107ad23b1627fe7cb5
GomezVictorM <victor.gomcha@gmail.com>
2020-08-11 11:37:33 +0200

    import libraries

---
+++ main.py
@@ -0,0 +1,8 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import dash_core_componentes as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
